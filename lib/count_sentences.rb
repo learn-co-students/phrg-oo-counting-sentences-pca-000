65a283b3fa00cffa08f7ddfe87ddf3b1bfd5260b
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-03-22 11:07:06 -0400

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = []
+    if self.length > 2
+      if self.include?(".")
+        count = self.split(/\. |\? |\! |\."|\?"|\!"/)
+      elsif self.include?("?")
+        count = self.split(/\. |\? |\! |\."|\?"|\!"/)
+      elsif self.include?("!")
+        count = self.split(/\. |\? |\! |\."|\?"|\!"/)
+      else
+        count = []
+      end
+    end
+  count.length
   end
 end

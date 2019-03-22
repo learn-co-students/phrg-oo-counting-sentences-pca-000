cba8c350ff68a6bc6e25225ae27e74fd12002594
KFad <kfadigan@yahoo.com>
2019-03-22 10:57:00 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.include?(".")
   end
 
   def question?
-
+    self.include?"?"
   end
 
   def exclamation?
-
+    self.include?"!"
   end
 
   def count_sentences
+    self.squeeze('.!?').count('.!?')
 
   end
 end
